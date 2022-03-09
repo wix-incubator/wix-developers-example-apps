b7b544aeafd8d815d55587a6962bc4865da09500
rono <rono@wix.com>
2022-03-09 21:55:18 +0200

    extending demo app

--- invoices-app/server/__tests__/utils.js
+++ invoices-app/server/__tests__/utils.js
@@ -4,5 +4,5 @@ const randomString = () => crypto.randomBytes(32).toString('base64')
 const randomUrl = () => `https://${randomString()}`;
 const randomObject = () => Object.assign({}, ...(new Array(10).fill(0).map(() => ({[randomString()]: randomString()}))))
 const randomPort = () => 10000 + Math.floor(Math.random() * 55000);
-
-module.exports = {randomString, randomUrl, randomObject, randomPort}
+const randomObjectWithField = (field, value) => Object.assign({}, {[field] : value} ,...(new Array(10).fill(0).map(() => ({[randomString()]: randomString()}))))
+module.exports = {randomString, randomUrl, randomObject, randomPort, randomObjectWithField}
