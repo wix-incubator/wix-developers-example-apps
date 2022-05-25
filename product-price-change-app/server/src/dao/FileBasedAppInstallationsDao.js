4aa628b74de971d32272e478c9bab907b23ab11f
rono <rono@wix.com>
2022-05-25 16:33:28 +0300

    ident

--- product-price-change-app/server/src/dao/FileBasedAppInstallationsDao.js
+++ product-price-change-app/server/src/dao/FileBasedAppInstallationsDao.js
@@ -1,17 +1,17 @@
-const {AppInstallationsDao} = require("./AppInstallationsDao");
+const { AppInstallationsDao } = require("./AppInstallationsDao");
 const fs = require('fs');
 
 class FileBasedAppInstallationsDao extends AppInstallationsDao {
     constructor() {
         super();
-            this.initStore = this.initStore.bind(this)
-    this.store = this.initStore()
+        this.initStore = this.initStore.bind(this)
+        this.store = this.initStore()
     }
 
     initStore() {
-    if (fs.existsSync("app_installs.localdb")) return JSON.parse(fs.readFileSync("app_installs.localdb"))
-    else { return {} }
-}
+        if (fs.existsSync("app_installs.localdb")) return JSON.parse(fs.readFileSync("app_installs.localdb"))
+        else { return {} }
+    }
     async save(instanceId, data) {
         this.store[instanceId] = data;
         fs.writeFileSync("app_installs.localdb", JSON.stringify(this.store))
