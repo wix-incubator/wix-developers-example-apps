994837b1669b9b04666d16aba8ea9797f5276fd0
rono <rono@wix.com>
2022-05-29 16:20:54 +0300

    added dao for buyers count addition

---
+++ product-price-change-app/server/src/dao/FileBasedBuyersCountDao.js
@@ -0,0 +1,28 @@
+const { BuyersCountDao } = require("./BuyersCountDao");
+const fs = require('fs');
+
+class FileBasedBuyersCountDao extends BuyersCountDao {
+    constructor() {
+        super();
+        this.initStore = this.initStore.bind(this)
+        this.store = this.initStore()
+    }
+
+    initStore() {
+        if (fs.existsSync("buyers_count.localdb")) return JSON.parse(fs.readFileSync("buyers_count.localdb"))
+        else { return {} }
+    }
+
+    async save(instanceId, buyersCount) {
+        this.store[instanceId] = buyersCount;
+        fs.writeFileSync("buyers_count.localdb", JSON.stringify(this.store))
+    }
+
+    async getBy(instanceId) {
+        return this.store[instanceId];
+    }
+}
+
+module.exports = {
+    FileBasedBuyersCountDao
+}
