560d937dc131eb3852cb35fab58a4ea723312a52
Alon Levi <alonl@wix.com>
2022-08-30 15:30:09 +0300

    remove code

--- invoices-app/client/src/pages/Dashboard/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as TotalContacts } from './TotalContacts';
-export { default as MainCard } from './MainCard';
-export { default as ListData } from './ListData';
--- invoices-app/server/__tests__/utils.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-const randomString = () => crypto.randomBytes(32).toString('base64')
-const randomUrl = () => `https://${randomString()}`;
-const randomObject = () => Object.assign({}, ...(new Array(10).fill(0).map(() => ({[randomString()]: randomString()}))))
-const randomPort = () => 10000 + Math.floor(Math.random() * 55000);
-
-module.exports = {randomString, randomUrl, randomObject, randomPort}
